5bfbdd94b34d07cd5ca736ef2cfbd1c2222db039
karrycgi <martin.karry@cgi.com>
2025-07-22 13:19:08 +0200

    Setup lint and test

--- packages/client-api/jest.config.mjs
+++ packages/client-api/jest.config.mjs
@@ -1,11 +1,13 @@
-const { createDefaultPreset } = require("ts-jest");
+import { createDefaultPreset } from "ts-jest"
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
-module.exports = {
+const jestConfig = {
   testEnvironment: "node",
   transform: {
     ...tsJestTransformCfg,
   },
 };
+
+export default jestConfig;
