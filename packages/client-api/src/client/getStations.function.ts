d45319f12ea4dc465e615d37cd4aed0e9f6f5bf6
karrycgi <martin.karry@cgi.com>
2025-07-23 13:35:50 +0200

    Fixxed lint any usage

--- packages/client-api/src/client/getStations.function.ts
+++ packages/client-api/src/client/getStations.function.ts
@@ -2,6 +2,5 @@ import { StationOverviewResult } from "../model/StationOverviewResult.zod";
 
 export async function getStations():Promise<StationOverviewResult> {
     const result = await fetch("https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations.json");
-    const json:any[] = await result.json();
-    return StationOverviewResult.parse(json);
+    return StationOverviewResult.parse(await result.json());
 }
