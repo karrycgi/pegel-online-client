6b687b99bdde6bbe6322f94ade064dba939ef2cb
karrycgi <martin.karry@cgi.com>
2025-07-23 16:16:53 +0200

    Fix

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export"
+  output: "export",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "/"
 };
 
 export default nextConfig;
