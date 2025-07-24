1105b3f9c5413a1b5a0caddc9e208b0e8b1cf3db
karrycgi <martin.karry@cgi.com>
2025-07-24 07:55:33 +0200

    Removed debugging output

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-console.log("!!!---!!!", process.env.NEXT_PUBLIC_BASE_PATH);
-
 const nextConfig: NextConfig = {
   output: "export",
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "/"
