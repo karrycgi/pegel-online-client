fcff04d3e13c41d8297ad6e642dffed40564b81b
karrycgi <martin.karry@cgi.com>
2025-07-24 07:31:49 +0200

    Fix base path

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+console.log("!!!---!!!", process.env.NEXT_PUBLIC_BASE_PATH);
+
 const nextConfig: NextConfig = {
-  output: "export"
+  output: "export",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "/"
 };
 
 export default nextConfig;
